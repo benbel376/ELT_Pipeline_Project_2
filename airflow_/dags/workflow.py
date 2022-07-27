0c9313d3039f96a05b60c676255e3fa7e1579a3a
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-07-27 23:12:19 +0300

    add task two in airflow

--- airflow_/dags/workflow.py
+++ airflow_/dags/workflow.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul  3 01:54:51 2021
-
-@author: viswa
-"""
-
 from airflow import DAG
 from datetime import datetime,timedelta
 from airflow.operators.bash_operator import BashOperator
@@ -14,10 +7,12 @@ import os
 import pandas as pd
 import json
 print(os.path.abspath("working.............................."))
-sys.path.append(os.path.abspath("includes"))
+sys.path.append(os.path.abspath("includes/python"))
 
 from Extractor import Extractor
+from Loader import Loader
 extract = Extractor()
+loader = Loader()
 
 def run_extractor(**context):
     sample = extract.load_csv("~/data/sample.csv")
@@ -26,6 +21,18 @@ def run_extractor(**context):
     cleaned.to_csv(path, index=False)
     context['ti'].xcom_push(key='dataframe', value=path)
     print(cleaned.iloc[1,1])
+
+def run_loader(**context):
+    path = context['ti'].xcom_pull(key='dataframe')
+    data = extract.load_csv(path)
+    print(f"..................  {data.columns}")
+    try:
+        conn, cur = loader.connect_to_server(host="postgres", port=5432, user="warehouse", password="warehouse", dbName="warehouse")
+        loader.create_table(cur, "includes/sql/create_source_table.sql", "warehouse")
+        loader.insert_into_table(cur, conn, "warehouse", data, "source")
+        loader.close_connection(cur, conn)
+    except Exception as e:
+        print(f"error...: {e}")
     
     
 
@@ -38,5 +45,10 @@ with DAG(dag_id="workflow",default_args=default_args,schedule_interval='@daily',
         python_callable = run_extractor,
         provide_context=True
         )
+    load_task = PythonOperator(
+        task_id = "load_task",
+        python_callable = run_loader,
+        provide_context=True
+        )
 
-extract_task
+extract_task >> load_task
