0aec8a4b2f83bfdbcd37c76becf81ecd3f6c935d
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-07-30 22:49:32 +0300

    update dreadme

--- airflow_/redash_migrator.py
+++ airflow_/redash_migrator.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from airflow import DAG
+from datetime import datetime,timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime as dt
